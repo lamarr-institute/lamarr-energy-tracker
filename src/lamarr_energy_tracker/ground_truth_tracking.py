29958782b0c84365748168b6e2da2660b9360f0b
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2026-03-17 09:44:50 +0000

    updated versions and fixed minor things

--- src/lamarr_energy_tracker/ground_truth_tracking.py
+++ src/lamarr_energy_tracker/ground_truth_tracking.py
@@ -71,7 +71,7 @@ class GroundTruthTrackingRequestHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(
-                json.dumps('   '.join(list(self.server.config.keys()))).encode()
+                json.dumps(list(self.server.config.keys()))
             )
             return
 
